b7fed9b1c6b8127203425bff7a23d4e5189cdb54
Bartlomiej Kozal <bkzl@me.com>
2016-08-26 22:18:56 +0200

    Set up view controllers

---
+++ MessagesExtension/MenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.swift
+//  Checkers
+//
+//  Created by bkzl on 26/08/16.
+//  Copyright © 2016 bkzl. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+}
